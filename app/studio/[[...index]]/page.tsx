7c87445d6eeb2a46d52fa775853c67a4f1a085bf
aerissat <aerissat@gmail.com>
2025-06-24 20:22:51 +0300

    `generateStaticParams()`

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -7,3 +7,8 @@ import config from "@/sanity.config";
 export default function Studio() {
   return <NextStudio config={config} />;
 }
+
+// Required for static export of catch-all dynamic route
+export async function generateStaticParams() {
+  return [];
+}
